8038a28afa2f730e30dab8fb232df9e922aa6aeb
Steve Hathaway <shathaway7@verizon.net>
2012-11-15 07:09:05 -0500

    Improve the README file

--- first_app/config/initializers/secret_token.rb
+++ first_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '82b723f403ace415943dc776037b38a66eb164c6125a824cd0977c0fca975dcc5b920e7f67f0656573b0f2d0f1eb5cc4d5172e72676828ca6ca994a50f1027d4'
+FirstApp::Application.config.secret_token = '8a27f8de565c232c2380d0594071e14711fc4ed2b99f84d1fc7815a973da3f780b81be4bcc2872702e4714743abf28a53b97b0879733e5c99cf279c882c23264'
